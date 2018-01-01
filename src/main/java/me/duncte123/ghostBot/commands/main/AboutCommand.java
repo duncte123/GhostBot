ff8d9529c99a9fef09c81358d4785d3ac0a37758
Duncan Sterken <duncan.sterken123@gmail.com>
2018-01-01 17:55:56 +0100

    Add invite link

--- src/main/java/me/duncte123/ghostBot/commands/main/AboutCommand.java
+++ src/main/java/me/duncte123/ghostBot/commands/main/AboutCommand.java
@@ -12,7 +12,7 @@ public class AboutCommand implements Command {
                 "I'm being developed by duncte123 (duncte123#1245) and if you have any ideas of what to add to me you can contact him or join [this server](https://discord.gg/NKM9Xtk)\n\n" +
 
                 "**Useful information:**\n" +
-                "Invite link: []()" +
+                "Invite link: [https://discordapp.com/oauth2/authorize?client_id=397297702150602752&scope=bot&permissions=8](https://discordapp.com/oauth2/authorize?client_id=397297702150602752&scope=bot&permissions=8)\n" +
                 "Prefix: " + Variables.PREFIX + "\n" +
                 "Support server: [https://discord.gg/NKM9Xtk](https://discord.gg/NKM9Xtk)\n" +
                 "Amount of servers that I'm in: " + event.getJDA().getGuildCache().size()
