f1049c2f2f49ab3b6856eb5520a50317f49f3a52
Duncan Sterken <duncan.sterken123@gmail.com>
2018-02-21 09:49:25 +0100

    Fix buildconfig

--- src/main/java/me/duncte123/ghostBot/variables/Variables.java
+++ src/main/java/me/duncte123/ghostBot/variables/Variables.java
@@ -19,10 +19,11 @@
 package me.duncte123.ghostBot.variables;
 
 import me.duncte123.ghostBot.utils.SpoopyUtils;
+import me.duncte123.ghostBot.BuildConfig;
 
 public class Variables {
 
     public static final String PREFIX = SpoopyUtils.config.getString("discord.prefix", "gb.");
     public static final String OWNER_ID = "191231307290771456";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = BuildConfig.VERSION;
 }
