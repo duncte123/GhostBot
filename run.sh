70cf13df82e3d2c8a3f98fb4469b495b9e4a6bce
duncte123 <contact@duncte123.me>
2021-03-31 08:55:40 +0200

    Update copyright

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-file=$(find | grep '^\./GhostBot-' | head -1)
-command="java -jar $file"
-
-echo "Runing $command"
-${command}
--- src/main/java/me/duncte123/ghostbot/objects/tumblr/TumblrPhoto.java
+++ src/main/java/me/duncte123/ghostbot/objects/tumblr/TumblrPhoto.java
@@ -1,6 +1,6 @@
 /*
  *     GhostBot, a Discord bot made for all your Danny Phantom needs
- *     Copyright (C) 2018 - 2019  Duncan "duncte123" Sterken
+ *     Copyright (C) 2018 - 2021  Duncan "duncte123" Sterken
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
